bdd154c3a5a92e445cc03877858ba2104792e8e9
王鹏 <wp@192.168.7.56>
2019-07-29 23:22:20 +0800

    Initial commit

---
+++ 20190729_V1.0_helloworld/src/com/kero99/wp/Test.java
@@ -0,0 +1,11 @@
+package com.kero99.wp;
+
+/**
+ * @author wp
+ * @date 2019-07-29 22:16
+ */
+public class Test {
+    public static void demo(){
+        System.out.println("hello,world");
+    }
+}
---
+++ 20190729_V1.1_for/src/com/kero99/wp/Demo.java
@@ -0,0 +1,11 @@
+package com.kero99.wp;
+
+/**
+ * @author wp
+ * @date 2019-07-29 22:30
+ */
+public class Demo {
+    public static void main(String [] args){
+//        Test.demo();
+    }
+}
